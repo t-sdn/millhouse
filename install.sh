d028d7b4720903a89437605c741b8c320c4443fa
kjwon15 <kjwonmail@gmail.com>
2015-08-05 13:11:28 +0900

    Remove docker-registry

--- install.sh
+++ install.sh
@@ -21,27 +21,6 @@ check_packages() {
     return 0
 }
 
-get_private_ip() {
-    ips=$(ifconfig | awk '/inet addr/{print substr($2,6)}')
-    for ip in $ips; do
-        if [ $ip == '10.*.*.*' ]; then
-            echo $ip
-            return 0
-        elif [ $ip == '192.168.*.*' ]; then
-            echo $ip
-            return 0
-        elif [ $ip == '172.*.*.*' ]; then
-            second_field=$(cut -d. -f2 <<< $ip)
-            if [ $second_field -ge 16 -a $second_field -le 31 ]; then
-                echo $ip
-                return 0
-            fi
-        fi
-    done
-
-    return 1
-}
-
 if ! check_packages docker.io jenkins; then
     echo "Add jenkins repository."
     wget -qO- https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
@@ -83,52 +62,8 @@ ExecStop=/usr/bin/docker stop gitlab-ce
 WantedBy=local.target
 EOF
 
-cat > /etc/systemd/system/jenkins-docker.service << EOF
-[Unit]
-Description=Jenkins ci in docker.
-After=docker.service
-Requires=docker.service
-
-[Service]
-Restart=always
-ExecStartPre=-/usr/bin/docker pull jenkins
-ExecStartPre=-/usr/bin/docker rm jenkins-ci
-ExecStart=/usr/bin/docker run --rm \
-          --name jenkins-ci \
-          --volume /srv/jenkins:/var/jenkins_home \
-          --publish 8080:8080 \
-          jenkins
-ExecStop=/usr/bin/docker stop jenkins-ci
-
-[Install]
-WantedBy=local.target
-EOF
-
-bind_ip=$(get_private_ip || echo '0.0.0.0')
-
-cat > /etc/systemd/system/docker-registry.service << EOF
-[Unit]
-Description=Private docker registry.
-After=docker.service
-Requires=docker.service
-
-[Service]
-Restart=always
-ExecStartPre=-/usr/bin/docker pull registry
-ExecStartPre=-/usr/bin/docker rm docker-registry
-ExecStart=/usr/bin/docker run --rm \
-          --name docker-registry \
-          --volume /srv/registry:/tmp/registry \
-          --publish $bind_ip:5000:5000 \
-          registry
-ExecStop=/usr/bin/docker stop docker-registry
-
-[Install]
-WantedBy=local.target
-EOF
-
-systemctl enable jenkins gitlab-docker docker-registry
-systemctl start jenkins gitlab-docker docker-registry
+systemctl enable jenkins gitlab-docker
+systemctl start jenkins gitlab-docker
 
 echo "Installing jenkins plugins."
 until wget -qO /dev/null http://localhost:8080/; do
